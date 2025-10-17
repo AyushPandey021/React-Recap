9548e1959fe7825d761920589a77d7b446e46786
ayushpandey4480@gmamil.com <ayushpandey4480@gmail.com>
2025-10-17 19:39:47 +0530

    img updated

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
       buttonText: "Click Here",
-      bgColor: "bg-red-200",
+      bgimg: "https://plus.unsplash.com/premium_photo-1760531730393-f210acf4d104?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600",
     },
     {
       title1: "Ayush Pandey",
@@ -16,7 +16,7 @@ const Home = () => {
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
       buttonText: "Apdev",
-      bgColor: "bg-green-200",
+      bgimg: "https://images.unsplash.com/photo-1760625525477-f725e48f5a13?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600",
     },
     {
       title1: "AI Analytics",
@@ -24,7 +24,7 @@ const Home = () => {
       description:
         "Analyze customer data efficiently and get predictive insights.",
       buttonText: "Explore",
-      bgColor: "bg-yellow-200",
+      bgimg: "https://plus.unsplash.com/premium_photo-1760614424094-4625f04d287d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600",
     },
   ];
 
@@ -39,13 +39,15 @@ const Home = () => {
         </h1>
       </div>
 
-      {/* Right Section (Dynamic Cards) */}
+   
       <div className="w-2/3 flex gap-6 overflow-x-auto no-scrollbar">
         {cards.map((card, index) => (
-          <div
+          <div>
+          <img
+          src={card.bgimg }
             key={index}
-            className={`min-w-[100px] ${card.bgColor} p-4 rounded-2xl shadow-lg  transition-all duration-300`}
-          >
+            className={`min-w-[100px] p-4 rounded-2xl shadow-lg  transition-all duration-300`}
+          />
             <h1 className="text-3xl font-semibold">{card.title1}</h1>
             <h2 className="text-3xl font-semibold mt-1">{card.title2}</h2>
             <p className="text-lg mt-4 text-gray-700">{card.description}</p>
